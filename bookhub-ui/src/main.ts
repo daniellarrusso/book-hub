8afc00861abcf5df00a435c78730dc1006adcdeb
daniellarrusso <npadams21@gmail.com>
2025-08-25 22:32:56 +0100

    install element plus design framework

--- bookhub-ui/src/main.ts
+++ bookhub-ui/src/main.ts
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import 'element-plus/theme-chalk/display.css';
+import './style.css';
 
-createApp(App).mount('#app')
+createApp(App).use(ElementPlus).mount('#app');
+
+createApp(App).mount('#app');
