ae93cb10a904c51f82f95cef71746c50cc5a476b
daniellarrusso <npadams21@gmail.com>
2025-08-26 13:37:50 +0100

    style: add list view basic design

--- bookhub-ui/src/components/BookList.vue
+++ bookhub-ui/src/components/BookList.vue
@@ -9,7 +9,7 @@
     </div>
   </section>
    <el-row :gutter="10" style="margin-bottom: 1rem;">
-    <el-col :span="16">
+    <el-col :span="18">
       <el-input v-model="search" @input="searchByTerm" placeholder="Search by title or author" clearable />
     </el-col>
       <el-col :span="4">
@@ -17,8 +17,13 @@
         <el-option label="Title" value="title" @click="toggleSort" />
       </el-select>
     </el-col>
+    <el-col :span="2">
+      <GridToggle @show-grid="val => isGridView = val" />
+    </el-col>
   </el-row>
   <el-skeleton v-if="isLoading" :rows="25" animated />
+  <div v-if="isGridView">GridView</div>
+  <div v-else>ListView</div>
   <ul>
     <li v-for="book in response?.items">{{ book.title }}</li>
   </ul>
@@ -31,6 +36,7 @@
   import bookService from '../services/api/books';
   import { ElMessage } from 'element-plus';
   import { useDebounceFn } from '@vueuse/core';
+  import GridToggle from './GridToggle.vue';
 
   const response = ref<PagedResponse<Book>>()
   const search = ref<string>('');
@@ -39,6 +45,7 @@
   const ascending = ref(true);  
   const isLoading = computed(() => bookService.isLoading.value)
   const error = computed(() => bookService.error.value)
+  const isGridView = ref(false);
 
   watch(error, (newError) => {
     if (newError) {
---
+++ bookhub-ui/src/components/GridToggle.vue
@@ -0,0 +1,23 @@
+<!-- GridToggle.vue -->
+<template>
+  <el-button-group>
+    <el-button :type="!showGrid ? 'primary' : ''" :icon="List" @click="toggleGridView" />
+    <el-button :type="showGrid ? 'primary' : ''" :icon="Grid" @click="toggleGridView" />
+  </el-button-group>
+</template>
+
+<script setup lang="ts">
+import { List, Grid } from '@element-plus/icons-vue'
+import { ref, defineEmits } from 'vue'
+
+const showGrid = ref(false)
+
+const emit = defineEmits<{
+  (e: 'showGrid', value: boolean): void
+}>()
+
+const toggleGridView = () => {
+  showGrid.value = !showGrid.value
+  emit('showGrid', showGrid.value)
+}
+</script>
