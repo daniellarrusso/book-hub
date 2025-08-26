1923bd38474ee2a1a09c5be4f07f52b19f5dda45
daniellarrusso <npadams21@gmail.com>
2025-08-26 07:49:44 +0100

    add: vitest to project. -simple test added

--- bookhub-ui/vite.config.ts
+++ bookhub-ui/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  test: {
+    globals: true,           // so you don’t need to import describe/it/expect
+    environment: 'jsdom',    // simulate browser environment
+    setupFiles: './src/setupTests.ts', // optional, for global test setup
+  },
+});
