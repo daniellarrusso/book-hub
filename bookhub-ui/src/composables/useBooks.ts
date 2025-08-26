5184eda03568ef7123aedc77418528f7407e847f
daniellarrusso <npadams21@gmail.com>
2025-08-26 15:37:13 +0100

    feat: add composable to separate load logic from parent component

---
+++ bookhub-ui/src/composables/useBooks.ts
@@ -0,0 +1,28 @@
+import { ref, computed, watch } from 'vue';
+import bookService from '../services/api/books';
+import type { Book } from '../types/book';
+import type { PagedResponse } from '../types/paged-response';
+
+export function useBooks() {
+  const response = ref<PagedResponse<Book>>();
+  const search = ref('');
+  const sort = ref<'asc' | 'desc'>('asc');
+
+  const isLoading = computed(() => bookService.isLoading.value);
+  const error = computed(() => bookService.error.value);
+
+  const fetchBooks = async (page = 1) => {
+    response.value = await bookService.getBooks(page, search.value, sort.value);
+  };
+
+  watch([search, sort], () => fetchBooks());
+
+  return {
+    response,
+    search,
+    sort,
+    isLoading,
+    error,
+    fetchBooks,
+  };
+}
