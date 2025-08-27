c807ff2198d4cf39a722e2c06f1764f5a2d123b4
daniellarrusso <npadams21@gmail.com>
2025-08-27 07:16:29 +0100

    test: add verbs to httpClient mock oin setup

--- bookhub-ui/src/setupTests.ts
+++ bookhub-ui/src/setupTests.ts
@@ -4,10 +4,16 @@ import httpClient from './services/http';
 // Cast httpClient so TypeScript knows about mocks
 export const mockedHttpClient = httpClient as unknown as {
   get: ReturnType<typeof vi.fn>;
+  post: ReturnType<typeof vi.fn>;
+  put: ReturnType<typeof vi.fn>;
+  delete: ReturnType<typeof vi.fn>;
 };
 
 // Set a default mock for all tests
 mockedHttpClient.get = vi.fn();
+mockedHttpClient.post = vi.fn();
+mockedHttpClient.delete = vi.fn();
+mockedHttpClient.put = vi.fn();
 
 // Optional: reset mocks before each test automatically
 beforeEach(() => {
